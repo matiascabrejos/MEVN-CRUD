ef058dd5cc0788aadcb5509a189b217e2459fcc4
matiascabrejos <matias.cabrejos@hotmail.com>
2022-02-06 19:55:28 -0300

    Setting up the schema of the db

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("API Tasks testing!");
+const Task = require("../models/Task");
+
+router.get("/", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
 });
 
 module.exports = router;
